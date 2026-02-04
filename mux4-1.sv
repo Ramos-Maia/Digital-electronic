132925cd90ca697af66d94e69c9b6032a1811795
Ramos-Maia <dmaiaramos11@gmail.com>
2026-02-04 18:12:36 -0400

    Add files via upload

---
+++ mux4-1.sv
@@ -0,0 +1,17 @@
+module mux4_1 (
+    input  logic [3:0] d,
+    input  logic [1:0] sel,
+    output logic y
+);
+
+    always_comb begin
+        case (sel)
+            2'b00: y = d[0];
+            2'b01: y = d[1];
+            2'b10: y = d[2];
+            2'b11: y = d[3];
+            default: y = 1'b0;
+        endcase
+    end
+
+endmodule
