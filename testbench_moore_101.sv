7de05a00743f8f4b79a5d4bef585077c62bb53d0
Ramos-Maia <dmaiaramos11@gmail.com>
2026-03-04 21:06:39 -0400

    Update testbench_moore_101.sv

--- testbench_moore_101.sv
+++ testbench_moore_101.sv
@@ -46,10 +46,25 @@ module testbench_moore_101;
         $finish;
     end
 
+    // Variável para mostrar o estado como string
+    logic [8*3-1:0] state_str; // 3 caracteres (S0, S1, S2, S3)
+
+    // Atualiza a string do estado
+    always_comb begin
+        case (uut.state)
+            uut.S0: state_str = "S0";
+            uut.S1: state_str = "S1";
+            uut.S2: state_str = "S2";
+            uut.S3: state_str = "S3";
+            default: state_str = "??";
+        endcase
+    end
+
     // Monitor
     initial begin
-        $display("Time\tclk\trst_n\tx\tz");
-        $monitor("%0t\t%b\t%b\t%b\t%b", $time, clk, rst_n, x, z);
+        $display("Time\tclk\trst_n\tx\tstate\tz");
+        $monitor("%0t\t%b\t%b\t%b\t%s\t%b",
+                 $time, clk, rst_n, x, state_str, z);
     end
 
     // Gerar waveform
